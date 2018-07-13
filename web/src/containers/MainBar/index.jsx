395f9ed09898d37961a5cc7902b5dcb2d0192342
Dilven <mateusiakkrystian@gmail.com>
2018-07-13 11:43:06 +0200

    fix layout

--- web/src/containers/MainBar/index.jsx
+++ web/src/containers/MainBar/index.jsx
@@ -22,7 +22,7 @@ class MainBar extends Component {
         <Toolbar>
         <IconButton
             color="inherit"
-            style={{ display: 'none' }}
+            style={{ display: '' }}
             aria-label="open drawer"
             onClick={toggleMenu.bind(this, true)}
             className='hamburger-button'
